a9066c0b20ba83a841ab009fcb45c6741a93dc02
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-04-20 10:44:37 +0200

    Still doesn't work

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,16 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
+val kotlin_version: String by project
+
+val exposed_version: String by project
+val h2_version: String by project
+
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.jetbrains.exposed:exposed-core:0.49.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.49.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.49.0")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
     implementation("io.ktor:ktor-client-serialization:2.3.7")
