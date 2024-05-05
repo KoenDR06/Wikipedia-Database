1eaf689f8763b47bd086492998aa79f7c3eb9e14
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-05-05 15:17:22 +0200

    Changed Main-Class

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest {
-        attributes("Main-Class" to "me.koendev.CommitToDBKt")
+        attributes("Main-Class" to "me.koendev.database.CommitToDBKt")
     }
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
